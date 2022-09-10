7e55f88659c3eec7e919bc675ebbf39029a69ccb
Sandson <Sandson.contato@gmail.com>
2022-09-10 00:05:57 -0300

    Quase acabado
    Que luta que foi

--- faq-feedback/raiz.js
+++ faq-feedback/raiz.js
@@ -1,3 +1,15 @@
 function Info(x){
-
-}
+    let informacao = document.getElementsByClassName("conteudo")[x]
+    let seta = document.getElementsByClassName("seta")[x]
+    let titulo = document.getElementsByClassName("titulo")[x]
+    if(informacao.style.display == "none" && seta.style.transform == "rotate(180deg)"){
+        informacao.style.display = "block"
+        seta.style.transform = "rotate(0deg)"
+        titulo.style.fontWeight = "900"
+    }
+    else{
+        informacao.style.display = "none"
+        seta.style.transform = "rotate(180deg)"
+        titulo.style.fontWeight = "350"
+    }
+} 
